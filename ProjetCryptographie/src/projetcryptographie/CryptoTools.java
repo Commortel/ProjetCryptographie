2d91f99b9da691659e7e04b05058f5d7a151ec0c
Commortel <commortel@gmail.com>
2013-04-02 16:31:47 +0200

    Starting IHM and CryptoTools

---
+++ ProjetCryptographie/src/projetcryptographie/CryptoTools.java
@@ -0,0 +1,18 @@
+package projetcryptographie;
+
+/**
+ *
+ * @author Thibaut
+ */
+public class CryptoTools 
+{
+    public static String GetSHA1(String str)
+    {
+        return null;
+    }
+    
+    public static String getMD5(String str)
+    {
+        return null;
+    }
+}
---
+++ ProjetCryptographie/src/projetcryptographie/ProjetCryptographie.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projetcryptographie;
+
+/**
+ *
+ * @author Thibaut
+ */
+public class ProjetCryptographie 
+{
+    public static void main(String[] args) 
+    {
+        // TODO code application logic here
+    }
+}
